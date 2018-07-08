17f4be09056a09984e2a644fa5ea847c601ad971
Edouard-Legoupil <edouard.legoupil@gmail.com>
2018-07-08 15:41:12 +0300

    rev slidify

--- slidify.R
+++ slidify.R
@@ -1,7 +1,12 @@
 #require(devtools)
 #install_github("slidify", "ramnathv")
 #install_github("slidifyLibraries", "ramnathv")
+
+#install_github("ramnathv/slidify")
+#install_github("ramnathv/slidifyLibraries")
+
 library(slidify)
+
 #author("slides")
 slidify("slides/index.Rmd")
 
